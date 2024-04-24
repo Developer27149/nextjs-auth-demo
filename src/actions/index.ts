59939421ad9a3a2577b73343f496947bbf05a422
Developer27149 <564151947@qq.com>
2024-04-25 01:53:17 +0800

    init

---
+++ src/actions/index.ts
@@ -0,0 +1,11 @@
+'use server'
+
+export const getUserInfoAction = async (): Promise<{
+    name?: string
+}> => {
+    return { name: undefined }
+}
+
+export const loginAction = () => { }
+
+export const registerAction = () => { }
